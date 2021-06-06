cc751bbe56bba65cadf10bc1f96df2d58a742818
AbraaoVilela <abr.alves3125@gmail.com>
2021-06-05 22:22:26 -0300

    Exclui o bom dia no começo da impressão de texto do CONSOLE. Criei o método calcular área modo curto e movi o código que estava em main para o método. Alterei o método main, agora chama os dois métodos calcular* que passaram também a ser static. Criamos novos métodos de contagem (contar até dez e contagem regressiva), além de mostrar como utilizar a classe scanner para ler o que o usuário escolheu no teclado.

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,16 +3,80 @@ package intro;
 
 // 2 - Referência às bibliotecas
 
+import java.util.Scanner;
+
 // 3 - Classe
 public class Medidas {
     // 3.1 Atributos - Características
 
     // 3.2 Métodos e Funções
     public static void main(String[] args) {
-        calcularAreaModoCurto();
-        calcularAreaModoExtenso();
+        // Condicional - Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
 
-            }
+        // utilizar a classe Scanner do Java para ler a escolha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U    D E   O P Ç Õ E S");
+        System.out.println("c - Calcular Area Modo Curto");
+        System.out.println("d - Contar até Dez");
+        System.out.println("e - Calcular Area Modo Extenso");
+        System.out.println("i - IfSimples");
+        System.out.println("r - Contagem Regressiva de 10 a 0");
+        System.out.println("Digite a opção desejada: ");
+        String opcao = scanner.next();
+        // switch = selecionar o comportamento do programa conforme a escolha da pessoa ou do software
+
+        //String opcao = "curto";
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método calcularAreaModoCurto");
+                calcularAreaModoCurto();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método contarAteDez");
+                contarAteDez();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            case "r":
+            case "R":
+                System.out.println("Você escolheu executar o método Contagem Regressiva");
+                contagemregressiva();
+                break;
+            default:
+                System.out.println("Você escolheu outro valor que não tem uma ação associada");
+                break;
+
+        }
+
+
+    }
+    public static void ifSimples(){
+        // Condicional - Verificar uma condição - Fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // if = se
+
+        String modo = "curto";
+
+        if (modo == "curto"){
+            calcularAreaModoCurto();
+        }
+
+        else {
+            calcularAreaModoExtenso();
+        }
+
+    }
 
     public static void calcularAreaModoCurto() {
         System.out.println("Cálculo de áreas - Modo Curto");
@@ -41,4 +105,21 @@ public class Medidas {
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + resultado + "m²");
     }
+   public static void contarAteDez(){
+
+        // Loops ou Repetições
+        // for = rpetição incondicional
+       System.out.println("Contagem Crescente");
+
+       for(int numero = 1; numero <= 10; numero++){
+           System.out.println(numero);
+       }
+   }
+   public static void contagemregressiva(){
+       System.out.println("Contagem Regressiva");
+
+       for(int numero = 10; numero >= -1; numero--){
+           System.out.println(numero);
+       }
+   }
 }
